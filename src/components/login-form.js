58ba51d665389e5a923b24cb4474a4934bc14a21
celedituro <cdituro@fi.uba.ar>
2022-09-11 14:35:03 -0300

    feat: design and estructure improved

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -1,49 +1,87 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { TextInput, Button, Checkbox, Menu, Text, Appbar, Navbar, FAB, RadioButton } from 'react-native-paper';
+import React, { useState } from "react";
+import { View, StyleSheet,CheckBox, Text } from 'react-native';
+import { TextInput, Button, Colors, Title } from 'react-native-paper';
 
-const MyComponent = () => {
-    const [checked, setChecked] = React.useState(false);
+const App = () => {
+  const [isSelected, setSelection] = useState(false);
 
-    return (
-    <Checkbox
-        status={checked ? 'checked' : 'unchecked'}
-        onPress={() => {
-        setChecked(!checked);
-        }}
-    />
-    );
-};
-
-export default () => {
-    return(
-        <View style={signUpStyle.container}>
-            <TextInput
+  return (
+        <View style={loginStyle.container}>
+            <Text style={loginStyle.title}>Login</Text>
+            <TextInput style={loginStyle.input}
                 mode="outlined"
                 label="Correo electrónico"
                 placeholder="Correo electrónico"
             />
-            <TextInput
+            <TextInput 
+                style={loginStyle.input}
+                secureTextEntry={true}
                 mode="outlined"
                 label="Contraseña"
                 placeholder="Contraseña"
             />
-            <Button style={signUpStyle.button} mode="contained" onPress={() => console.log('Pressed')}>
+            <View style={loginStyle.checkboxContainer}>
+                <CheckBox
+                    color={Colors.blue800}
+                    value={isSelected}
+                    onValueChange={setSelection}
+                    style={loginStyle.checkbox}
+                />
+                <Text style={loginStyle.label}>Recordarme</Text>
+            </View>
+            <Button style={loginStyle.button} color={Colors.blue800} mode="contained" onPress={() => console.log('Pressed')}>
                 Iniciar sesión
             </Button>
+        <View style={loginStyle.subcontainerRedes}>
+            <Text style={loginStyle.label}>O iniciar sesión con</Text>
+            <Button style={loginStyle.button} color={Colors.red800} mode="contained" onPress={() => console.log('Pressed')}>
+                Google
+            </Button>
+            <Button style={loginStyle.button} color={Colors.green800} mode="contained" onPress={() => console.log('Pressed')}>
+                WhatsApp
+            </Button>
         </View>
-    );
+    </View>
+  );
 };
 
-const signUpStyle = StyleSheet.create({
+const loginStyle = StyleSheet.create({
     container: {
-        flex: 1,
         justifyContent: 'center',
         alignContent: 'center',
-        padding: 15
+        margin: 30,
+        padding: 100,
+        backgroundColor: '#f5f5f5',
+    },
+    title: {
+        fontSize: 50,
+        fontStyle: 'bold',
+        paddingLeft: 450,
+        paddingBottom: 30,
+    },
+    input: {
+        marginTop: 5,
+        marginBottom: 5,
+    },
+    checkboxContainer: {
+        flexDirection: "row",
+        margin: 10,
+    },
+    checkbox: {
+        alignSelf: "center",
+    },
+    label: {
+        margin: 8,
+        fontSize: 'medium',
+    },
+    subcontainerRedes: {
+        marginTop: 20,
+        alignItems: 'center',
     },
     button: {
-        marginTop:5,
-        padding:7
+        marginTop: 15,
+        padding: 5,
     },
 });
+
+export default App;
