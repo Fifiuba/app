c353033e7ff5292fa5c647022ddc73538b398ccf
celedituro <cdituro@fi.uba.ar>
2022-09-11 18:10:32 -0300

    feat: validation at login added

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -1,26 +1,73 @@
 import { React, useState } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { TextInput, Button, Colors } from 'react-native-paper';
+import {useForm, Controller} from 'react-hook-form';
 import CheckBox from 'expo-checkbox';
 
 const App = () => {
-
     const [isSelected, setSelection] = useState(false);
+    const [hidePassword, sethidePassword] = useState(true);
+    const {control, formState: {errors}} = useForm({
+        defaultValues: {
+        email: '',
+        password: '',
+        },
+    });
+
     return (
         <View style={loginStyle.container}>
             <Text style={loginStyle.title}>Login</Text>
-            <TextInput style={loginStyle.input}
-                mode="outlined"
-                label="Correo electrónico"
-                placeholder="Correo electrónico"
+            <Controller control={control}
+                rules={{
+                required: true,
+                validate: isValidEmail,
+                maxLength: constraints.email.max}}
+                render={({field: {onChange, onBlur, value}}) => (
+                <TextInput
+                    onBlur={onBlur}
+                    onChangeText={onChange}
+                    value={value}
+                    mode="outlined"
+                    label="Correo electrónico"
+                    placeholder="Correo electrónico"
+                    right={<TextInput.Affix text={'/' + constraints.email.max} />}
+                />
+                )}
+                name="email"
             />
-            <TextInput 
-                style={loginStyle.input}
-                secureTextEntry={true}
-                mode="outlined"
-                label="Contraseña"
-                placeholder="Contraseña"
+            {errors.email?.type === 'required' &&
+            <Text>Campo requerido</Text>}
+            {errors.email?.type === 'validate' &&
+            <Text>Ingrese un correo electrónico válido</Text>}
+            <Controller control={control}
+                rules={{
+                    required: true,
+                    maxLength: constraints.password.max,
+                    minLength: constraints.password.min}}
+                render={({field: {onChange, onBlur, value}}) => (
+                    <TextInput
+                        onBlur={onBlur}
+                        onChangeText={onChange}
+                        value={value}
+                        mode="outlined"
+                        label="Contraseña"
+                        placeholder="Contraseña"
+                        secureTextEntry={hidePassword}
+                        right={
+                        <TextInput.Icon
+                            icon="eye"
+                            onPress={() => sethidePassword(!hidePassword)}
+                        />}
+                    />
+                )}
+                name="password"
             />
+            {errors.password?.type === 'required' &&
+            <Text>Campo requerido</Text>}
+            {errors.password?.type === 'maxLength' &&
+            <Text>Máximo {constraints.password.max}</Text>}
+            {errors.password?.type === 'minLength' &&
+            <Text>Mínimo {constraints.password.min}</Text>}
             <View style={loginStyle.checkboxContainer}>
                 <CheckBox
                     color={Colors.blue800}
@@ -33,26 +80,37 @@ const App = () => {
             <Button style={loginStyle.button} color={Colors.blue800} mode="contained" onPress={() => console.log('Pressed')}>
                 Iniciar sesión
             </Button>
-        <View style={loginStyle.subcontainerRedes}>
-            <Text style={loginStyle.label}>O iniciar sesión con</Text>
-            <Button style={loginStyle.button} color={Colors.red800} mode="contained" onPress={() => console.log('Pressed')}>
-                Google
-            </Button>
-            <Button style={loginStyle.button} color={Colors.green800} mode="contained" onPress={() => console.log('Pressed')}>
-                WhatsApp
-            </Button>
-        </View>
-        <View style={{margin: 20}}>
-            <Text style={{marginTop: 10, textAlign:'center'}}>
-                ¿Olvidaste la contraseña?
-                <Text style={{textDecorationLine: 'underline'}}>
-                    Registrate
+            <View style={loginStyle.subcontainerRedes}>
+                <Text style={loginStyle.label}>O iniciar sesión con</Text>
+                <Button style={loginStyle.button} color={Colors.red800} mode="contained" onPress={() => console.log('Pressed')}>
+                    Google
+                </Button>
+                <Button style={loginStyle.button} color={Colors.green800} mode="contained" onPress={() => console.log('Pressed')}>
+                    WhatsApp
+                </Button>
+            </View>
+            <View style={{margin: 20}}>
+                <Text style={{marginTop: 10, textAlign:'center'}}>
+                    ¿Olvidaste la contraseña?
+                    <Text style={{textDecorationLine: 'underline'}}>
+                        Registrate
+                    </Text>
                 </Text>
-            </Text>
+            </View>
         </View>
-    </View>
-  );
+    );
 };
+
+const constraints = {
+  name: {max: 15, min: 3},
+  email: {max: 50},
+  password: {min: 8, max: 20},
+};
+
+const isValidEmail = (email) =>
+  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email,
+);
 
 const loginStyle = StyleSheet.create({
     container: {
