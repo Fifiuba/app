08ff1f0ea259f301b832ac48bed98504ba612329
celedituro <cdituro@fi.uba.ar>
2022-09-24 10:12:54 -0300

    refactor: separate profile in component and view

--- src/components/unlogged-nav.js
+++ src/components/unlogged-nav.js
@@ -2,12 +2,26 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SignUpView from '../views/sign-up-view';
 import LoginView from '../views/login-view';
+import HomeView from '../views/home-view';
+import ProfileView from '../views/profile-view';
 
 const Stack = createNativeStackNavigator();
 
 export default function UnloggedNav(props) {
   return (
     <Stack.Navigator>
+      <Stack.Screen
+        name="Home">
+        {(navigation) =>
+          <HomeView onNavigation={navigation}/>
+        }
+      </Stack.Screen>
+      <Stack.Screen
+        name="Profile">
+        {(navigation) =>
+          <ProfileView onNavigation={navigation}/>
+        }
+      </Stack.Screen>
       <Stack.Screen
         name="Login">
         {(navigation) =>
