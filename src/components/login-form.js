4c98f9a75c4ac04610681d09a3ef36580b1329c6
celedituro <cdituro@fi.uba.ar>
2022-10-04 16:32:58 -0300

    fix: fix problems with react-natie version and move initializeApp to app file

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -3,27 +3,12 @@ import {View, StyleSheet, Text} from 'react-native';
 import {TextInput, Button, Colors} from 'react-native-paper';
 import {useForm, Controller} from 'react-hook-form';
 import login from '../services/login';
+import { getAuth, GoogleAuthProvider, signInWithCredential } from 'firebase/auth';
 
 import * as React from 'react';
 import * as WebBrowser from 'expo-web-browser';
 import * as Google from 'expo-auth-session/providers/google';
-import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider, signInWithCredential } from 'firebase/auth';
 import {WEB_CLIENT_ID} from '@env'
-
-const firebaseConfig = {
-  apiKey: "AIzaSyD_fAr5j7fbdhezakqnBmSLW2xEZ9Uki2U",
-  authDomain: "user-service-9def8.firebaseapp.com",
-  databaseURL: "https://user-service-9def8-default-rtdb.firebaseio.com",
-  projectId: "user-service-9def8",
-  storageBucket: "user-service-9def8.appspot.com",
-  messagingSenderId: "627165168741",
-  appId: "1:627165168741:web:23447b751047a7ca24b28c",
-  measurementId: "G-YL8STET37V"
-};
-
-// Initialize Firebase
-initializeApp({firebaseConfig});
 
 WebBrowser.maybeCompleteAuthSession();
 
