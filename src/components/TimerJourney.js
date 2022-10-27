a71301825ba65f76c2165398178ee5717e991cac
celedituro <cdituro@fi.uba.ar>
2022-10-27 16:14:41 -0300

    refactor

--- src/components/TimerJourney.js
+++ src/components/TimerJourney.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
- 
+
 import {
   SafeAreaView,
   StyleSheet,
@@ -7,50 +7,50 @@ import {
   View,
   TouchableHighlight,
 } from 'react-native';
- 
+
 import {Stopwatch, Timer} from 'react-native-stopwatch-timer';
- 
+
 const TimerJourney = () => {
   const [isStopwatchStart, setIsStopwatchStart] = useState(false);
   const [resetStopwatch, setResetStopwatch] = useState(true);
 
   return (
-      <View style={styles.container}>
-        <View style={styles.sectionStyle}>
-          <Stopwatch
-            laps
-            secs
-            start={isStopwatchStart}
-            // To start
-            reset={resetStopwatch}
-            // To reset
-            options={options}
-            // Options for the styling
-            getTime={() => {}}
-          />
-          <TouchableHighlight
-            onPress={() => {
-              setIsStopwatchStart(!isStopwatchStart);
-              setResetStopwatch(false);
-            }}>
-            <Text style={styles.buttonText}>
-              {!isStopwatchStart ? 'START' : 'STOP'}
-            </Text>
-          </TouchableHighlight> 
-          <TouchableHighlight
-            onPress={() => {
-              setIsStopwatchStart(false);
-              setResetStopwatch(true);
-            }}>
-            <Text style={styles.buttonText}>RESET</Text>
-          </TouchableHighlight>
-        </View>
+    <View style={styles.container}>
+      <View style={styles.sectionStyle}>
+        <Stopwatch
+          laps
+          secs
+          start={isStopwatchStart}
+          // To start
+          reset={resetStopwatch}
+          // To reset
+          options={options}
+          // Options for the styling
+          getTime={() => {}}
+        />
+        <TouchableHighlight
+          onPress={() => {
+            setIsStopwatchStart(!isStopwatchStart);
+            setResetStopwatch(false);
+          }}>
+          <Text style={styles.buttonText}>
+            {!isStopwatchStart ? 'START' : 'STOP'}
+          </Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          onPress={() => {
+            setIsStopwatchStart(false);
+            setResetStopwatch(true);
+          }}>
+          <Text style={styles.buttonText}>RESET</Text>
+        </TouchableHighlight>
       </View>
+    </View>
   );
 };
- 
+
 export default TimerJourney;
- 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -63,7 +63,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
- 
+
 const options = {
   container: {
     backgroundColor: 'grey',
--- src/views/RoadTripView.js
+++ src/views/RoadTripView.js
@@ -1,53 +1,53 @@
-import React, { useContext, useState } from 'react';
+import React, {useContext, useState} from 'react';
 import {View, StyleSheet} from 'react-native';
 import MapView, {Marker, Polyline} from 'react-native-maps';
-import { Avatar, Text, Button, Colors } from 'react-native-paper';
+import {Avatar, Text, Button, Colors} from 'react-native-paper';
 import * as Location from 'expo-location';
 
 import {UserContext} from '../context/UserContext';
-import { constants } from '../utils/Constants';
+import {constants} from '../utils/Constants';
 import TimerJourney from '../components/TimerJourney';
 
 const RoadTripView = () => {
-    //const journeyInfo = route.params;
-    //const journeyRoute = journeyInfo.route;
-    //const journeyDestination = journeyInfo.destination;
+  // const journeyInfo = route.params;
+  // const journeyRoute = journeyInfo.route;
+  // const journeyDestination = journeyInfo.destination;
 
-    //const user = useContext(UserContext);
-    //const userInfo = user.userInfo;
-    
-    const [origin, setOrigin] = useState({
-        latitude: -34.59908,
-        longitude: -58.38186,
-    });
-    const [route, setRoute] = useState([origin, destination]);
+  // const user = useContext(UserContext);
+  // const userInfo = user.userInfo;
 
-    const getLocationPermission = async () => {
-        try {
-            const {status} = await Location.requestForegroundPermissionsAsync();
+  const [origin, setOrigin] = useState({
+    latitude: -34.59908,
+    longitude: -58.38186,
+  });
+  const [route, setRoute] = useState([origin, destination]);
 
-            if (status !== 'granted') {
-                alert('Permiso denegado');
-                return;
-            }
+  const getLocationPermission = async () => {
+    try {
+      const {status} = await Location.requestForegroundPermissionsAsync();
 
-            const location = await Location.getCurrentPositionAsync({});
-            const current = {
-                latitude: location.coords.latitude,
-                longitude: location.coords.longitude,
-            };
-            setOrigin(current);
-        } catch (error) {
-            console.error(error.message);
-            return null;
-        }
-    };
+      if (status !== 'granted') {
+        alert('Permiso denegado');
+        return;
+      }
 
-    React.useEffect(() => {
-        getLocationPermission();
-    }, []);
+      const location = await Location.getCurrentPositionAsync({});
+      const current = {
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      };
+      setOrigin(current);
+    } catch (error) {
+      console.error(error.message);
+      return null;
+    }
+  };
 
-    /*const [journeyIsFinished, setJourneyIsFinished] = useState(true);
+  React.useEffect(() => {
+    getLocationPermission();
+  }, []);
+
+  /* const [journeyIsFinished, setJourneyIsFinished] = useState(true);
     const [isStopwatchStart, setIsStopwatchStart] = useState(true);
     const [resetStopwatch, setResetStopwatch] = useState(false);
 
@@ -58,101 +58,101 @@ const RoadTripView = () => {
         }
     });*/
 
-    const handleCancelJourney = async () => {
-        try {
-          const response = await cancelJourney(journeyInfo);
-          console.log('response:', response);
-          if (!(response === null)) {
-            alert('Viaje cancelado exitosamente');
-            navigation.navigate('Home');
-          } else {
-            alert('Error al cancelar viaje');
-          }
-        } catch (error) {
-          console.error(error.message);
-        }
-    };
+  const handleCancelJourney = async () => {
+    try {
+      const response = await cancelJourney(journeyInfo);
+      console.log('response:', response);
+      if (!(response === null)) {
+        alert('Viaje cancelado exitosamente');
+        navigation.navigate('Home');
+      } else {
+        alert('Error al cancelar viaje');
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
 
-    return (
-        <>
-        <View style={styles.appBar}>
-            <View style={styles.profile}>
-                <Avatar.Image size={45} source={constants.DEFAULT_URL_USER_PICTURE} />
-                <Text>Celeste Dituro</Text>
-            </View>
+  return (
+    <>
+      <View style={styles.appBar}>
+        <View style={styles.profile}>
+          <Avatar.Image size={45} source={constants.DEFAULT_URL_USER_PICTURE} />
+          <Text>Celeste Dituro</Text>
         </View>
-        <View style={styles.container}>
-            <TimerJourney
-                //isStopwatchStart={isStopwatchStart}
-                //resetStopwatch={resetStopwatch}
-            />
-            <MapView
-                style={styles.map}
-                region={{
-                    latitude: origin.latitude,
-                    longitude: origin.longitude,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
-                }}>
-                <Marker
-                    draggable
-                    coordinate={origin}
-                    onDragEnd={(direction) => setOrigin(direction.nativeEvent.coordinate)} />
-                <Marker
-                    draggable
-                    coordinate={destination}
-                    onDragEnd={(direction) => setDestination(direction.nativeEvent.coordinate)} />
-                <Polyline
-                    coordinates={route}
-                    strokeColor="red"
-                    strokeWidth={5} />
-            </MapView>
-            <Button
-                style={styles.button}
-                color={Colors.red800}
-                mode="contained"
-                onPress={() => {
-                    console.log('Cancel journey');
-                    handleCancelJourney();
-                }}>
-                <Text style={styles.buttonText}>Cancelar viaje</Text>
-            </Button>
-        </View>
-        </>
-    );
-}
+      </View>
+      <View style={styles.container}>
+        <TimerJourney
+          // isStopwatchStart={isStopwatchStart}
+          // resetStopwatch={resetStopwatch}
+        />
+        <MapView
+          style={styles.map}
+          region={{
+            latitude: origin.latitude,
+            longitude: origin.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}>
+          <Marker
+            draggable
+            coordinate={origin}
+            onDragEnd={(direction) => setOrigin(direction.nativeEvent.coordinate)} />
+          <Marker
+            draggable
+            coordinate={destination}
+            onDragEnd={(direction) => setDestination(direction.nativeEvent.coordinate)} />
+          <Polyline
+            coordinates={route}
+            strokeColor="red"
+            strokeWidth={5} />
+        </MapView>
+        <Button
+          style={styles.button}
+          color={Colors.red800}
+          mode="contained"
+          onPress={() => {
+            console.log('Cancel journey');
+            handleCancelJourney();
+          }}>
+          <Text style={styles.buttonText}>Cancelar viaje</Text>
+        </Button>
+      </View>
+    </>
+  );
+};
 
 export default RoadTripView;
 
 const styles = StyleSheet.create({
-    container: {
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    appBar:{      
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'flex-end',
-        height: '10%',
-        marginTop: 50,
-    },
-    profile:{
-        alignItems:'center',
-        padding: 5,
-    },
-    map: {
-        width: '95%',
-        height: '70%',
-    },
-    button: {
-        padding: 5,
-        width: 210,
-        height: 50,
-        marginTop: 30,
-    },
-    buttonText: {
-        fontSize: 16,
-        color: 'white',
-    },
- });
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  appBar: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+    height: '10%',
+    marginTop: 50,
+  },
+  profile: {
+    alignItems: 'center',
+    padding: 5,
+  },
+  map: {
+    width: '95%',
+    height: '70%',
+  },
+  button: {
+    padding: 5,
+    width: 210,
+    height: 50,
+    marginTop: 30,
+  },
+  buttonText: {
+    fontSize: 16,
+    color: 'white',
+  },
+});
 
