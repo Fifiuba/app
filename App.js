a05ca43eca03d0ff91f1901d91fc05166158866a
alejovillores <avillores@fi.uba.ar>
2022-11-16 17:59:51 -0300

    fix: App.js

--- App.js
+++ App.js
@@ -1,183 +1,75 @@
-// import React, {useState, useEffect, useRef} from 'react';
-// import { NavigationContainer } from '@react-navigation/native';
-// import LoggedNav from './src/components/LoggedNav';
-// import UnloggedNav from './src/components/UnloggedNav';
-// import './FirebaseConfig';
-// import * as Device from 'expo-device';
-// import * as Notifications from 'expo-notifications';
-// import {NotificationContext} from './src/context/NotificationContext';
-
-// Notifications.setNotificationHandler({
-//     handleNotification: async () => ({
-//       shouldShowAlert: true,
-//       shouldPlaySound: false,
-//       shouldSetBadge: false,
-//     }),
-// });
-
-
-// export default function App() {
-//   const [isLoggedIn, setIsLoggedIn] = useState(false)
-//   const [expoPushToken, setExpoPushToken] = useState('');
-//   const [notification, setNotification] = useState(false);
-//   const notificationListener = useRef();
-  
-//   const registerForPushNotificationsAsync = async () => {
-//       let token;
-
-//       if (Platform.OS === 'android') {
-//         await Notifications.setNotificationChannelAsync('default', {
-//           name: 'default',
-//           importance: Notifications.AndroidImportance.MAX,
-//           vibrationPattern: [0, 250, 250, 250],
-//           lightColor: '#FF231F7C',
-//         });
-//       }
-
-//       if (Device.isDevice) {
-//         const { status: existingStatus } = await Notifications.getPermissionsAsync();
-//         let finalStatus = existingStatus;
-//         if (existingStatus !== 'granted') {
-//           const { status } = await Notifications.requestPermissionsAsync();
-//           finalStatus = status;
-//         }
-//         if (finalStatus !== 'granted') {
-//           alert('Failed to get push token for push notification!');
-//           return;
-//         }
-
-//         token = (await Notifications.getExpoPushTokenAsync()).data;
-//         console.log(token);
-//       } else {
-//         alert('Must use physical device for Push Notifications');
-//       }
-//       return token;
-//   }
-
-
-//   const removeNotificationSubscription = (subs) => {
-//       Notifications.removeNotificationSubscription(subs);
-//   }
-
-//   useEffect(() => {
-//     registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
-
-//     notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
-//       //console.log(notification)
-//       setNotification(notification);
-//     });
-
-//     return () => {
-//       removeNotificationSubscription(notificationListener.current);
-//     };
-//   }, []);
-
-//   return (
-//     <NotificationContext.Provider value={notification}>
-//       <NavigationContainer>
-//         {isLoggedIn ? <LoggedNav onLogin={setIsLoggedIn}/> : <UnloggedNav onLogin={setIsLoggedIn}/>}
-//       </NavigationContainer>
-//     </NotificationContext.Provider>
-// )}
-
+import React, {useState, useEffect, useRef} from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import LoggedNav from './src/components/LoggedNav';
+import UnloggedNav from './src/components/UnloggedNav';
+import './FirebaseConfig';
 import * as Device from 'expo-device';
 import * as Notifications from 'expo-notifications';
-import React, { useState, useEffect, useRef } from 'react';
-import { Text, View, Button, Platform } from 'react-native';
+import {NotificationContext} from './src/context/NotificationContext';
 
 Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: false,
-    shouldSetBadge: false,
-  }),
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+    }),
 });
 
+
 export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
   const [expoPushToken, setExpoPushToken] = useState('');
   const [notification, setNotification] = useState(false);
   const notificationListener = useRef();
-  const responseListener = useRef();
-
-  useEffect(() => {
-    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
-
-    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
-      setNotification(notification);
-    });
-
-    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-      console.log(response);
-    });
-
-    return () => {
-      Notifications.removeNotificationSubscription(notificationListener.current);
-      Notifications.removeNotificationSubscription(responseListener.current);
-    };
-  }, []);
-
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'space-around',
-      }}>
-      <Text>Your expo push token: {expoPushToken}</Text>
-      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Title: {notification && notification.request.content.title} </Text>
-        <Text>Body: {notification && notification.request.content.body}</Text>
-        <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
-      </View>
-      <Button
-        title="Press to schedule a notification"
-        onPress={async () => {
-          await schedulePushNotification();
-        }}
-      />
-    </View>
-  );
-}
-
-async function schedulePushNotification() {
-  await Notifications.scheduleNotificationAsync({
-    content: {
-      title: "You've got mail! 📬",
-      body: 'Here is the notification body',
-      data: { data: 'goes here' },
-    },
-    trigger: { seconds: 2 },
-  });
-}
-
-async function registerForPushNotificationsAsync() {
-  let token;
-
-  if (Platform.OS === 'android') {
-    await Notifications.setNotificationChannelAsync('default', {
-      name: 'default',
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: '#FF231F7C',
-    });
-  }
-
-  if (Device.isDevice) {
-    const { status: existingStatus } = await Notifications.getPermissionsAsync();
-    let finalStatus = existingStatus;
-    if (existingStatus !== 'granted') {
-      const { status } = await Notifications.requestPermissionsAsync();
-      finalStatus = status;
+  
+   const registerForPushNotificationsAsync = async () => {
+       let token
+       if (Platform.OS === 'android') {
+         await Notifications.setNotificationChannelAsync('default', {
+           name: 'default',
+           importance: Notifications.AndroidImportance.MAX,
+           vibrationPattern: [0, 250, 250, 250],
+           lightColor: '#FF231F7C',
+         });
+       
+       if (Device.isDevice) {
+        const { status: existingStatus } = await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+        if (existingStatus !== 'granted') {
+          const { status } = await Notifications.requestPermissionsAsync();
+          finalStatus = status;}
+        if (finalStatus !== 'granted') {
+          alert('Failed to get push token for push notification!');
+          return;
+        }
+         
+        token = (await Notifications.getExpoPushTokenAsync()).data;
+        console.log(token);
+        } else {
+        alert('Must use physical device for Push Notifications');
+       }
+       return token;
+      }
     }
-    if (finalStatus !== 'granted') {
-      alert('Failed to get push token for push notification!');
-      return;
-    }
-    token = (await Notifications.getExpoPushTokenAsync()).data;
-    console.log(token);
-  } else {
-    alert('Must use physical device for Push Notifications');
-  }
+   const removeNotificationSubscription = (subs) => {
+       Notifications.removeNotificationSubscription(subs);
+   }
+   
+   useEffect(() => {
+     registerForPushNotificationsAsync().then(token => setExpoPushToken(token))
+     notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+       //console.log(notification)
+       setNotification(notification);
+     })
+     return () => {
+       removeNotificationSubscription(notificationListener.current);
+     };
+   }, [])
 
-  return token;
-}
+   return (
+     <NotificationContext.Provider value={notification}>
+       <NavigationContainer>
+         {isLoggedIn ? <LoggedNav onLogin={setIsLoggedIn}/> : <UnloggedNav onLogin={setIsLoggedIn}/>}
+       </NavigationContainer>
+     </NotificationContext.Provider>
+ )}
