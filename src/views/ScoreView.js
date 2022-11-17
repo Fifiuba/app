076fc9a69b21b024b7bf9a2abe53942249b240f5
celedituro <cdituro@fi.uba.ar>
2022-11-17 18:54:17 -0300

    refactor: scoreBar inside scoreView in order to have access to score

--- src/views/ScoreView.js
+++ src/views/ScoreView.js
@@ -1,11 +1,40 @@
 import React, {useState} from 'react';
-import {SafeAreaView, Text, StyleSheet, View} from 'react-native';
+import {SafeAreaView, Text, StyleSheet, View, Image, TouchableOpacity} from 'react-native';
 import {Colors, Button} from 'react-native-paper';
 
 import {ScoreBar} from '../components/ScoreBar';
 
 export default function ScoreView({navigation}) {
   const [text, setText] = useState('chofer');
+  const [defaultRating, setDefaultRating] = useState(2);
+  const [maxRating, setMaxRating] = useState([1, 2, 3, 4, 5]);
+  const starCornerUrl = 'https://github.com/tranhonghan/images/blob/main/star_corner.png?raw=true';
+  const starFilledUrl = 'https://github.com/tranhonghan/images/blob/main/star_filled.png?raw=true';
+
+  const ScoreBar = () => {
+    return (
+      <View style={styles.subcontainer}>
+        {
+          maxRating.map((item, key) => {
+            return (
+              <TouchableOpacity
+                activeOpacity={0.7}
+                key={item}
+                onPress={() => setDefaultRating(item)}>
+                <Image
+                  style={styles.starImage}
+                  source={
+                          item <= defaultRating ?
+                          {uri: starFilledUrl} :
+                          {uri: starCornerUrl}
+                  }
+                />
+              </TouchableOpacity>
+            );
+          })}
+      </View>
+    );
+  };
 
   /* React.useEffect(() => {
       const setUserType = async () => {
@@ -36,6 +65,7 @@ export default function ScoreView({navigation}) {
           }}>
           <Text style={styles.buttonText}>Calificar {text}</Text>
         </Button>
+        <Text style={styles.textScore}>{defaultRating + '/' + maxRating.length}</Text>
       </View>
     </SafeAreaView>
   );
@@ -76,5 +106,18 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     color: 'white',
+  },
+  subcontainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    margin: 30,
+  },
+  starImage: {
+    width: 30,
+    height: 30,
+    margin: 5,
+  },
+  textScore: {
+    fontSize: 16,
   },
 });
